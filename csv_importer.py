a716eea597f769392a91898d49d45de3970bad66
G <jeehyun.kim@students.olin.edu>
2015-03-08 03:00:39 -0400

    csv_importer updated

--- csv_importer.py
+++ csv_importer.py
@@ -1,17 +1,21 @@
-# import csv
-# DRG=[]
-# with open ('IPS') as csvfile:
-#with open ('short') as csvfile:
-	# reader=csv.DictReader(csvfile)
-	# for row in reader:
-	# 	if row.get('DRG Definition') not in DRG:
-	# 		DRG.append(row.get('DRG Definition'))
-	# print DRG
-	# csvfile.seek(0)
-	# reader=csv.DictReader(csvfile)
-	# for line in DRG:
-	# 	print line
-	# 	csvfile.seek(0)
-	# 	for row in reader:
-	# 		if row.get('DRG Definition')==line:
-	# 			pass
+"""
+Testing how cvs import works
+"""
+
+import csv
+
+DRG=[]
+with open ('Data/data_1') as csvfile:
+	reader=csv.DictReader(csvfile)
+	for row in reader:
+		if row.get('DRG Definition') not in DRG:
+			DRG.append(row.get('DRG Definition'))
+	print DRG
+	csvfile.seek(0)
+	reader=csv.DictReader(csvfile)
+	for line in DRG:
+		print line
+		csvfile.seek(0)
+		for row in reader:
+			if row.get('DRG Definition')==line:
+				pass
