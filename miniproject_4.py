11728e96ad9a4e31e1620b9bdc5f9c484f09fab8
Wilson Tang <wilson.tang@students.olin.edu>
2015-03-10 21:18:17 -0400

    Wilson's code

--- miniproject_4.py
+++ miniproject_4.py
@@ -34,7 +34,19 @@ def get_data (ticker):
 
 data = get_data('039 - EXTRACRANIAL PROCEDURES W/O CC/MCC')
 # print(data)
-map_options = GMapOptions(lat=float(data['Latitude'][0]), lng=float(data['Longtitude'][0]), zoom=15)
+# base = {"Provider Id": [], "Provider Name": [], "Provider Street Address": [], "Provider City": [], "Provider State": [], "Provider Zip Code": [], "Hospital Referral Region Description": [], "Total Discharges": [], "Average Covered Charges": [], "Average Total Payments": [], "Average Medicare Payments": [], "Latitude": [], "Longtitude": []}
+# keys = ["Provider Id","Provider Name","Provider Street Address","Provider City","Provider State","Provider Zip Code","Hospital Referral Region Description","Total Discharges","Average Covered Charges","Average Total Payments","Average Medicare Payments","Latitude","Longtitude"]
+
+# with open(filename) as f:
+#     reader = csv.DictReader(f)
+#     data = {}
+#     for line in reader:
+#         drgDict = data.get(line['DRG Definition'], base.copy())
+#         for key in keys:
+#             drgDict[key].append(line[key])
+#         data[line['DRG Definition']] = drgDict
+
+map_options = GMapOptions(lat=30.2861, lng=-97.7394, zoom=15)
 
 plot = GMapPlot(
     x_range=x_range, y_range=y_range,
@@ -43,6 +55,33 @@ plot = GMapPlot(
 )
 plot.map_options.map_type="hybrid"
 
+# print("START")
+# print(data)
+# print("END")
+# source = ColumnDataSource(
+#         data= data['039 - EXTRACRANIAL PROCEDURES W/O CC/MCC']
+#     )
+
+# test = data['039 - EXTRACRANIAL PROCEDURES W/O CC/MCC']
+# print(test['Latitude'])
+
+# source = ColumnDataSource(
+#     data=dict(
+#         lat = [float(i) for i in test['Latitude']],
+#         lon = [float(i) for i in test['Longtitude']],
+#         name = test['Provider Name'],
+#         referral = test['Hospital Referral Region Description'],
+#         discharges = test['Total Discharges'],
+#         covered = test['Average Covered Charges'],
+#         payments_total = test['Average Total Payments'],
+#         payments_medical = test['Average Medicare Payments'],
+#         street = test['Provider Street Address'],
+#         city = test['Provider City'],
+#         state = test['Provider State'],
+#         zip = test['Provider Zip Code']
+#     )
+# )
+
 source = ColumnDataSource(
     data=dict(
         lat = [float(i) for i in data['Latitude']],
@@ -50,9 +89,9 @@ source = ColumnDataSource(
         DRG = data['DRG Definition'],
         name = data['Provider Name'],
         referral = data['Hospital Referral Region Description'],
-        discharges = data[' Total Discharges '],
-        covered = data[' Average Covered Charges '],
-        payments_total = data[' Average Total Payments '],
+        discharges = data['Total Discharges'],
+        covered = data['Average Covered Charges'],
+        payments_total = data['Average Total Payments'],
         payments_medical = data['Average Medicare Payments'],
         street = data['Provider Street Address'],
         city = data['Provider City'],
@@ -60,9 +99,8 @@ source = ColumnDataSource(
         zip = data['Provider Zip Code']
     )
 )
-
 #create all the points based on source data
-circle = Circle(x='lon', y='lat', size=15, fill_color="blue", line_color="black")
+circle = Circle(x='lat', y='lon', size=15, fill_color="blue", line_color="black")
 plot.add_glyph(source, circle)
 
 #set and add interactive tools
@@ -71,12 +109,12 @@ wheel_zoom = WheelZoomTool()
 box_select = BoxSelectTool()
 hover = HoverTool()
 
-hover.tooltips = [
-    ("Zip Code"," @state @zip"),
-    ("Average Total Payments","@payments_total"),
-    ("Number of Discharges","@discharges")
+# hover.tooltips = [
+#     ("Zip Code"," @state @zip"),
+#     ("Average Total Payments","@payments_total"),
+#     ("Number of Discharges","@discharges")
 
-]
+# ]
 
 plot.add_tools(pan, wheel_zoom, box_select,hover)
 
